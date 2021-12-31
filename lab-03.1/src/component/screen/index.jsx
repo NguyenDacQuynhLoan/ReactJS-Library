ae87b3463d018f7b482cdc42ded34e0dd3717ac1
Loan Nguyen <loannguyen1456@gmail.com>
2022-01-01 02:31:37 +0700

    remove wrong folder

--- lab-03.1/src/component/screen/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './style.css'
-export default function Screen(props) {
-    return (
-        <input className='screen'
-             type='text' value={props.value} />
-    )
-}
